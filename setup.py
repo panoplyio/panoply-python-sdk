4075406865b1d8ded7c52512588b2a523de6da7f
herzaso <herzaso@gmail.com>
2017-06-26 10:14:21 +0300

    add some metadata to the setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
 from panoply import VERSION, PKGNAME
 setup(
-    name = PKGNAME,
-    version = VERSION,
+    name=PKGNAME,
+    version=VERSION,
     packages=["panoply"],
     install_requires=[
         "requests==2.3.0",
         "oauth2client==4.1.1"
     ],
-
+    url="https://github.com/panoplyio/panoply-python-sdk",
+    author="Panoply.io",
+    author_email="support@panoply.io"
 )
