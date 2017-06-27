a4391e83282edb9e629e2dd0ba3ee0e635f056a6
aviaoh <avia@panoply.io>
2017-06-27 10:15:08 +0300

    add test profile to setup

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
         "requests==2.3.0",
         "oauth2client==4.1.1"
     ],
+    extras_require={
+        "test": [
+            "pep8==1.7.0",
+            "coverage==4.3.4"
+        ]
+    },
     url="https://github.com/panoplyio/panoply-python-sdk",
     author="Panoply.io",
     author_email="support@panoply.io"
