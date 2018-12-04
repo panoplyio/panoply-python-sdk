ac8d6210b1e48e7ce1131412b45d23846b7c73d2
GregRos <greg.rosenbaum@panoply.io>
2018-12-04 11:21:35 +0200

    Fix to minor style issue

--- test.py
+++ test.py
@@ -7,8 +7,6 @@ SECRET = "MmM0NWNvc2wwYmJ4ZDJ0OS84MmY3MzQ4NC02MDIzLTQyN2QtODdkMS0yY2I0NTAzNDk0ND
 sdk = panoply.SDK(KEY, SECRET)
 sdk.write('roi-test', {'hello': 1})
 
-
-
 print sdk.qurl
 
 time.sleep(5)
