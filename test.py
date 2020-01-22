a4e7303b11d37607e35810085f696e0ea1746051
hanzarnante <hanz@panoply.io>
2020-01-22 15:03:26 +0800

    Added unit test

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 from unittest import TestCase
 import panoply
+import base64
 
-TEST_KEY = "key"
-TEST_SECRET = "MmM0NWNvc2wwYmJ4ZDJ0OS84MmY3MzQ4NC02MDIzLTQyN2QtODdkMS0yY2I0NTAzNDk0NDQvMDM3MzM1OTk5NTYyL3VzLWVhc3QtMQ=="  # noqa
+TEST_KEY = "test/key"
+TEST_SECRET = b"rand2/uuid/awsaccount/region"
 
 
 class TestPanoplyPythonSDK(TestCase):
-
     def test_init(self):
-        sdk = panoply.SDK(TEST_KEY, TEST_SECRET)
-        expected_url = "https://sqs.us-east-1.amazonaws.com/037335999562/sdk-key-2c45cosl0bbxd2t9"  # noqa
+        sdk = panoply.SDK(TEST_KEY, base64.b64encode(TEST_SECRET))
+        expected_url = "https://sqs.region.amazonaws.com/awsaccount/sdk-test-rand2"  # noqa
 
         self.assertEqual(sdk.qurl, expected_url)
+
+    def test_write(self):
+        sdk = panoply.SDK(TEST_KEY, base64.b64encode(TEST_SECRET))
+        sdk.write('table', {'data': 1})
+        sdk.write('table', {'data': 2})
+
+        self.assertEqual(sdk._buffer.qsize(), 2)
