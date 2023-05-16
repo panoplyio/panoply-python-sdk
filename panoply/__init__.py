4a5cfd10852c876ce096ee30ef31b2d67f58d388
andrey-panoply <andrey2panoply.io>
2023-05-16 17:49:14 +0300

    Print threading errors to stdout

--- panoply/__init__.py
+++ panoply/__init__.py
@@ -1,3 +1,28 @@
+from sys import stdout
+from traceback import print_exception as _print_exception
+
 from .datasource import *
 from .sdk import *
 from .ssh import SSHTunnel
+
+
+def custom_excepthook(args, /):
+    """
+    Handle uncaught Thread.run() exception
+    and print error text to STDOUT instead of STDERR.
+
+    "It's always assumed that
+    the runnner is single-threaded and synchronuous such that `result` events
+    are only assigned to the last executed request".
+    see https://github.com/panoplyio/legacy-source-wrapper/blob/master/src/sources-runner/index.js#L74
+    """
+    if args.exc_type == SystemExit:
+        # silently ignore SystemExit
+        return
+
+    print(f"Caught an exception in thread:")
+    _print_exception(args.exc_type, args.exc_value, args.exc_traceback,
+                     file=stdout)
+
+
+threading.excepthook = custom_excepthook
